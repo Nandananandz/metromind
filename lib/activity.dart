aa3a2bbccb9e4ac1b8e713459d5d2bbcf04c3097
Sabarinath P <sabarinath5604@gmail.com>
2023-12-08 22:08:51 +0530

    selection added

--- lib/activity.dart
+++ lib/activity.dart
@@ -2,137 +2,150 @@ import 'package:flutter/material.dart';
 import 'package:flutterapp/update.dart';
 
 class activityScreen extends StatefulWidget {
-  const activityScreen({super.key});
+  activityScreen({super.key});
 
   @override
   State<activityScreen> createState() => _activityScreenState();
 }
 
 class _activityScreenState extends State<activityScreen> {
+  int OptSelector = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          
           Container(
-            margin: EdgeInsets.fromLTRB(5, 30, 0, 0),
-            child: InkWell(
-              onTap: () {
-                Navigator.of(context).pop();
-              },
-              child: Icon(Icons.arrow_back_ios_new,size: 30,color: Colors.black,))),
-            SizedBox(height: 30,),
-            Column(
-              children: [
-                Align(
+              margin: EdgeInsets.fromLTRB(5, 30, 0, 0),
+              child: InkWell(
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Icon(
+                    Icons.arrow_back_ios_new,
+                    size: 30,
+                    color: Colors.black,
+                  ))),
+          SizedBox(
+            height: 30,
+          ),
+          Column(
+            children: [
+              Align(
                   alignment: Alignment.topLeft,
-
-                  child: Padding(padding: EdgeInsets.only(left: 13),
-                    child: Image.asset("assets/text2.png"))),
-                    SizedBox(height: 30,),
-          
-            
-            
-                 Container(width: 320,
-                 height: 100,
+                  child: Padding(
+                      padding: EdgeInsets.only(left: 13),
+                      child: Image.asset("assets/text2.png"))),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                width: 320,
+                height: 100,
                 decoration: BoxDecoration(
-                  image: DecorationImage(image: AssetImage("assets/rect1.png"),
-                  fit: BoxFit.fill )
-                
-                ),
-                
-                 child: Row(
+                    image: DecorationImage(
+                        image: AssetImage("assets/rect1.png"),
+                        fit: BoxFit.fill)),
+                child: Row(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    SizedBox(width: 34,),
+                    SizedBox(
+                      width: 34,
+                    ),
                     Image.asset("assets/Anxiety.png"),
                     Expanded(child: Container()),
                     Image.asset("assets/g10.png")
                   ],
                 ),
-                ),
-                   SizedBox(height: 12,),
-                   
-                 Container(width: 320,
-                 height: 100,
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Container(
+                width: 320,
+                height: 100,
                 decoration: BoxDecoration(
-                  image: DecorationImage(image: AssetImage("assets/rect2.png"),
-                  
-                  fit: BoxFit.fill )
-                
-                ),
-                 child: Row(
+                    image: DecorationImage(
+                        image: AssetImage("assets/rect2.png"),
+                        fit: BoxFit.fill)),
+                child: Row(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    SizedBox(width: 34,),
+                    SizedBox(
+                      width: 34,
+                    ),
                     Image.asset("assets/Sleep.png"),
                     Expanded(child: Container()),
                     Image.asset("assets/g134.png")
                   ],
                 ),
-                ),
-                
-                   SizedBox(height: 12,),
-                   
-                 Container(width: 320,
-                 height: 100,
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Container(
+                width: 320,
+                height: 100,
                 decoration: BoxDecoration(
-                  image: DecorationImage(image: AssetImage("assets/rect3.png"),
-                  fit: BoxFit.fill, )
-                
-                ),
-                 child: Row(
+                    image: DecorationImage(
+                  image: AssetImage("assets/rect3.png"),
+                  fit: BoxFit.fill,
+                )),
+                child: Row(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    SizedBox(width: 34,),
+                    SizedBox(
+                      width: 34,
+                    ),
                     Image.asset("assets/Stress.png"),
-                   
                     Image.asset("assets/stresspic.png")
                   ],
                 ),
-                ),
-                
-                   SizedBox(height: 12,),
-                    Container(width: 320,
-                 height: 100, decoration: BoxDecoration(
-                  image: DecorationImage(image: AssetImage("assets/rect4.png"),
-                  fit: BoxFit.fill, )
-                
-                ),
-                 child: Row(
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Container(
+                width: 320,
+                height: 100,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                  image: AssetImage("assets/rect4.png"),
+                  fit: BoxFit.fill,
+                )),
+                child: Row(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    SizedBox(width: 34,),
+                    SizedBox(
+                      width: 34,
+                    ),
                     Image.asset("assets/couple.png"),
                     Expanded(child: Container()),
                     Image.asset("assets/cpic.png")
                   ],
                 ),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              InkWell(
+                onTap: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => updateScreen()));
+                },
+                child: Container(
+                  width: 320,
+                  height: 55,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("assets/frame2.png"),
+                          fit: BoxFit.fill)),
                 ),
-                SizedBox(height: 24,),
-                   InkWell(
-                    onTap: () {
-                      Navigator.of(context).push(MaterialPageRoute(builder: (context) => updateScreen()));
-                    },
-                     child: Container(width: 320,
-                                    height: 55,
-                                   decoration: BoxDecoration(
-                                     image: DecorationImage(image: AssetImage("assets/frame2.png"),
-                                     fit: BoxFit.fill )
-                                   
-                                   ),
-                                  
-                                     ),
-                   )
-            
-          
-          
-              ],
-            ),
- 
-
+              )
+            ],
+          ),
         ],
       ),
     );
--- lib/update.dart
+++ lib/update.dart
@@ -9,6 +9,21 @@ class updateScreen extends StatefulWidget {
 }
 
 class _updateScreenState extends State<updateScreen> {
+  List OptionList = [
+    "Happy",
+    "Sad",
+    "Calm",
+    "Angry",
+    "Stressed",
+    "Anixous",
+    "Excited",
+    "Annoyed"
+  ];
+
+  String selected = "";
+
+  List SelectedList = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,115 +31,131 @@ class _updateScreenState extends State<updateScreen> {
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-            Container(
-            margin: EdgeInsets.fromLTRB(5, 30, 0, 0),
-            child: InkWell(
-              onTap: () {
-                Navigator.of(context).pop();
-              },
-              child: Icon(Icons.arrow_back_ios_new,size: 30,color: Colors.black,))),
-            SizedBox(height: 20,),
-             Align(
-              alignment: Alignment.center,
-               child: Container(width: 230,
-                   height: 330,
-                   alignment: Alignment.bottomCenter,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(image: AssetImage("assets/Ellipse.png"),
-                    fit: BoxFit.fitWidth,)
-                    ),
-                    child: Column(
-                      children: [
-                        Container(
-                          width: 300,
-                          height: 300,
-                          alignment: Alignment.topCenter,
-                          child: Image.asset("assets/Group.png",)),  ],
-                    ),
-                  ),
-                  
-             ),
-             Container(
-                            width: 500,
-              margin: EdgeInsets.only(left: 20),
-              child: Image.asset("assets/text3.png"),
-             ),
-             SizedBox(height: 2,),
-            Padding(
-              padding: EdgeInsets.all(8),
-              child: Wrap(
-             
+          Container(
+              margin: EdgeInsets.fromLTRB(5, 30, 0, 0),
+              child: InkWell(
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Icon(
+                    Icons.arrow_back_ios_new,
+                    size: 30,
+                    color: Colors.black,
+                  ))),
+          SizedBox(
+            height: 20,
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Container(
+              width: 230,
+              height: 330,
+              alignment: Alignment.bottomCenter,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                image: AssetImage("assets/Ellipse.png"),
+                fit: BoxFit.fitWidth,
+              )),
+              child: Column(
                 children: [
-                  
-                  
-                   Container(
-                    margin: EdgeInsets.fromLTRB(8,15,0,0),
-                    child: Image.asset("assets/Frame4.png")),
-                       
                   Container(
-                     margin: EdgeInsets.fromLTRB(8,15,0,0),
-                    child: Image.asset("assets/Frame5.png")),
-                
-                  Container(
-                     margin: EdgeInsets.fromLTRB(8,15,0,0)
-                     ,child: Image.asset("assets/Frame6.png")),
-                 
-                  Container( margin: EdgeInsets.fromLTRB(8,15,0,0),
-                    child: Image.asset("assets/Frame7.png")),
-                   
-                  Container( margin: EdgeInsets.fromLTRB(8,15,0,0),
-                    child: Image.asset("assets/Frame8.png")),
-                  
-            
-                  Container(margin: EdgeInsets.fromLTRB(8,15,0,0),
-                    child: Image.asset("assets/Frame9.png")),
-                
-                    
-                  Container(margin: EdgeInsets.fromLTRB(60,15,0,0),
-                    child: Image.asset("assets/Frame10.png")),
-                  SizedBox(width: 5,height: 3),
-                  Container(margin: EdgeInsets.fromLTRB(8,15,0,0),
-                    child: Image.asset("assets/Frame11.png")),
-                    
-                ], ),
-            ),
-            Row(
-              children: [
-                Expanded(child: Container()),
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Image.asset("assets/skip.png")
-                  
-                  ),
-                  SizedBox(width: 20,),
-              ],
-            ),
-            Container(
-              margin: EdgeInsets.only(left: 13),
-              child: Image.asset("assets/img.png")),
-              SizedBox(height: 5,),
-            Container(
-              width: 400,
-              height: 15,
-              margin: EdgeInsets.only(left: 15),
-              child: Stack(
-                children: [
-                    Image.asset("assets/Line1.png"),
-              Image.asset("assets/Line2.png"),
+                      width: 300,
+                      height: 300,
+                      alignment: Alignment.topCenter,
+                      child: Image.asset(
+                        "assets/Group.png",
+                      )),
                 ],
               ),
             ),
-           SizedBox(height: 10,),
-            InkWell(
-              onTap: () {
-                Navigator.of(context).push(MaterialPageRoute(builder: (context) => nextScreen()));
-              },
-              
-              child: Container(alignment: Alignment.center,
-                child: Image.asset("assets/next.png")),
+          ),
+          Container(
+            width: 500,
+            margin: EdgeInsets.only(left: 20),
+            child: Image.asset("assets/text3.png"),
+          ),
+          SizedBox(
+            height: 2,
+          ),
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: Wrap(
+              runSpacing: 15,
+              spacing: 10,
+              alignment: WrapAlignment.center,
+              crossAxisAlignment: WrapCrossAlignment.center,
+              runAlignment: WrapAlignment.center,
+              children: [
+                // loop to execute option
+                for (var data in OptionList)
+                  InkWell(
+                    onTap: () {
+                      setState(() {
+                        if (SelectedList.indexOf(data) > -1)
+                          SelectedList.remove(data);
+                        else {
+                          SelectedList.add(data);
+                        }
+
+                        // assign value selected for finding which is selected
+                      });
+                    },
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 29, vertical: 5.5),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(40),
+                          color: (SelectedList.indexOf(data) > -1)
+                              ? Color(0xff009789)
+                              : Color(0xffF2F2F2)),
+                      child: Text(data),
+                    ),
+                  )
+              ],
             ),
-            
-            ],),
+          ),
+          Row(
+            children: [
+              Expanded(child: Container()),
+              Align(
+                  alignment: Alignment.topRight,
+                  child: Image.asset("assets/skip.png")),
+              SizedBox(
+                width: 20,
+              ),
+            ],
+          ),
+          Container(
+              margin: EdgeInsets.only(left: 13),
+              child: Image.asset("assets/img.png")),
+          SizedBox(
+            height: 5,
+          ),
+          Container(
+            width: 400,
+            height: 15,
+            margin: EdgeInsets.only(left: 15),
+            child: Stack(
+              children: [
+                Image.asset("assets/Line1.png"),
+                Image.asset("assets/Line2.png"),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => nextScreen()));
+            },
+            child: Container(
+                alignment: Alignment.center,
+                child: Image.asset("assets/next.png")),
+          ),
+        ],
+      ),
     );
   }
 }
